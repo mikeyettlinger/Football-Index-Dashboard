c5d9d98de0abddb0a6f16854354cb1ef49ac8e22
Mikey Ettlinger <mikeyettlinger@gmail.com>
2020-07-13 23:42:39 +0100

    response.body = portfolio but as a STRING

--- Desktop/plab-ror/app/controllers/portfolios_controller.rb
+++ Desktop/plab-ror/app/controllers/portfolios_controller.rb
@@ -6,32 +6,58 @@ class PortfoliosController < ApplicationController
 
     require 'webdrivers'
 
-    browser = Watir::Browser.new
-    browser.goto('https://www.footballindex.co.uk/top-200?login=true')
-    # Login
-    modal = browser.div(id: 'modal-container').wait_until_present
-    modal.text_field(placeholder: "Email address").set 'mikeyettlinger@gmail.com'
-    modal.text_field(placeholder: "Password").set 'mikey125'
-    modal.button(id: 'login').click
+    # browser = Watir::Browser.new
+    # browser.goto('https://www.footballindex.co.uk/top-200?login=true')
+    # # Login
+    # modal = browser.div(id: 'modal-container').wait_until_present
+    # modal.text_field(placeholder: "Email address").set 'mikeyettlinger@gmail.com'
+    # modal.text_field(placeholder: "Password").set 'mikey125'
+    # modal.button(id: 'login').click
 
-    links = []
+    # links = []
 
-    # find the 'Portfolio button'
-    browser.as.each do |link|
-      links << link
-    end
+    # # find the 'Portfolio button'
+    # browser.as.each do |link|
+    #   links << link
+    # end
+
+    # browser.goto(links[12].href)
+
+    # divs = []
+
+    # links[12].href.divs.each do |div|
+    #   if divs.class == "Row__row___qFVrH"
+    #     divs << div
+    #   end
+
+    # raise
+
+    # end
+
+    require 'json'
+    require 'rest-client'
+
+
+    url = 'https://api-prod.footballindex.co.uk/portfolio'
+
+    @response = RestClient.get(url, host: "api-prod.footballindex.co.uk", method: :get, content_type: "application/json", accept: "application/json", "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhhcnJ5YWNodXJjaGlsbEBnbWFpbC5jb20iLCJpZCI6NDU3OTc3LCJncm91cCI6InVzZXJzIiwiZGlzcGxheU5hbWUiOiJIYXJyeSBDIiwiaWF0IjoxNTkzNTk5MDQzfQ.6Bv67lhyf1DuWzbJp68oYqf5tGxSXBaMnOZF1fj79dI")
+
+
+
+
+
 
     # click the 'Portfolio button'
-    links[12].wait_until_present.click
+    # links[12].click
 
-    @portfolio = []
-    players = []
+    # @portfolio = []
+    # players = []
 
-    browser.divs.each do |div|
-      if div.class == "Pic__container__name__26P8X".wait_until(&:present?)
-         @portfolio << div.text
-      end
-    end
+    # browser.divs.each do |div|
+    #   if div.class == "Pic__container__name__26P8X".wait_until(&:present?)
+    #      @portfolio << div.text
+    #   end
+    # end
 
 end
 
--- Desktop/plab-ror/app/javascript/packs/application.js
+++ Desktop/plab-ror/app/javascript/packs/application.js
@@ -33,6 +33,10 @@ require("packs/apex-charts/custom-chart")
 require("packs/custom")
 require ("packs/edge")
 
+
+
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
