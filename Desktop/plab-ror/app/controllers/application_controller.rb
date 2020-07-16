12840c975f129651dbf2382347eae8467be2c939
Mikey Ettlinger <mikeyettlinger@gmail.com>
2020-07-16 11:52:03 +0100

    fixing this

--- Desktop/plab-ror/app/controllers/application_controller.rb
+++ Desktop/plab-ror/app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
     include DashboardsHelper
+<<<<<<< HEAD
     include MyPort
       before_action :load_port, :load_transactions, :load_player
+=======
+>>>>>>> 9566ea77b30166b0a3922b45c0fd50c6f012d007
 end
